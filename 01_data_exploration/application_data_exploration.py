95aa0f5a4561cc64b2f857e587ec3f930f8763d4
Heidi Sairanen <hasair88@gmail.com>
2020-11-06 00:03:47 +0200

    Data exploration init

---
+++ 01_data_exploration/application_data_exploration.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  5 23:56:46 2020
+
+@author: heidi
+"""
+
+import numpy as np
+import pandas as pd
+
+
+#%% Get data
+
+df = pd.read_csv('./00_raw_data/application_train.csv')
+
